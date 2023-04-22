a4b00d169d506498c44956787ed7baf4d572bc12
Ben Purinton <ben.purinton@gmail.com>
2023-04-21 16:33:52 -0800

    working

--- app.rb
+++ app.rb
@@ -4,5 +4,19 @@ require 'binding_of_caller'
 require "sinatra/reloader" if settings.development?
 
 get '/' do
-  "<h1>Hello world!</h1>"
+  # "<h1>Hello world!</h1>"
+  # settings.public_folder
+  send_file('public/index.html')
+end
+
+get '/rock' do
+  send_file('public/rock.html')
+end
+
+get '/paper' do
+  send_file('public/paper.html')
+end
+
+get '/scissors' do
+  send_file('public/scissors.html')
 end
